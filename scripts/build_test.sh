1040a7dd8c3f75acdf7cd56af2f101c351823de8
Aaron Buchwald <aaron.buchwald56@gmail.com>
2022-04-27 20:47:41 -0400

    Run unit tests in sequence

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -9,4 +9,4 @@ AVALANCHE_NETWORK_RUNNER_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. &&
 # Load the versions
 source "$AVALANCHE_NETWORK_RUNNER_PATH"/scripts/install_avalanchego.sh
 
-go test -timeout="600s" -coverprofile="coverage.out" -covermode="atomic" ./...
+go test -timeout="600s" -coverprofile="coverage.out" -covermode="atomic" ./... -p 1
